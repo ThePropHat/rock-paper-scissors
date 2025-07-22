1bc436c6cfbfff4b2ef18c2f14de4fb14ad941f5
ThePropHat <85534637+ThePropHat@users.noreply.github.com>
2025-07-21 20:23:27 -0600

    Improve R/P/S logic

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -3,15 +3,15 @@ const choiceScissors = 2;
 const choicePaper = 3;
 
 function getComputerChoice(){
-    let randomNumber = Math.floor(Math.random() * 100) + 1; //Generate random number
+    let randomNumber = Math.floor(Math.random() * 3) + 1; //Generate random number
 
-    if (randomNumber <= 33){ //Check for numbers under 33
+    if (randomNumber === 1){ //Check for number 1
         return choiceRock
 
-    } else if(randomNumber >= 66){ //Check for numbers over 66
+    } else if(randomNumber === 2){ //Check for number 2
         return choicePaper
 
-    } else(){ //Check for numbers in between 33 and 66
+    } else(){ //Check for number 3
         return choiceScissors
 
     }
