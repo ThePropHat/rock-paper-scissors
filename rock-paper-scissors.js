073f75192d521f362c4fffa60e2186d679b19075
ThePropHat <85534637+ThePropHat@users.noreply.github.com>
2025-07-22 08:58:33 -0600

    Reduce math

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,18 +1,15 @@
 function getComputerChoice(){
-    let randomNumber = Math.floor(Math.random() * 3) + 1; //Generate random number 1-3
+    const randomNumber = Math.floor(Math.random() * 3); //Generate random number 0-2
 
-    if (randomNumber===1){
+    if (randomNumber===0){
         return "Rock";
     }
 
-    else if (randomNumber===2){
+    else if (randomNumber===1){
         return "Scissors";
-
     }
    
     else{
         return "Paper";
     }
 }
-
-getComputerChoice()
