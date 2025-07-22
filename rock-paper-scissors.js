42be9dfe94f6849160b5739ff8dd258f19e4b7f6
ThePropHat <85534637+ThePropHat@users.noreply.github.com>
2025-07-22 08:35:35 -0600

    Add random number gen

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -3,19 +3,8 @@ const choiceScissors = 2;
 const choicePaper = 3;
 
 function getComputerChoice(){
-    let randomNumber = Math.floor(Math.random() * 3) + 1; //Generate random number
-
-    if (randomNumber === 1){ //Check for number 1
-        return choiceRock
-
-    } else if(randomNumber === 2){ //Check for number 2
-        return choicePaper
-
-    } else(){ //Check for number 3
-        return choiceScissors
-
-    }
- 
+    let randomNumber = Math.floor(Math.random() * 3) + 1; //Generate random number 1-3
+    console.log(randomNumber)
    
 }
 
