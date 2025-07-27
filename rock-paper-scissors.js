d7ab5a51bd746846ad0afc1f965f3ad63341c6af
ThePropHat <85534637+ThePropHat@users.noreply.github.com>
2025-07-27 16:00:20 -0600

    Simplify choice logic

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,7 +1,4 @@
 
-
-
-
 function getComputerChoice(){
     const randomNumber = Math.floor(Math.random() * 3); //Generate random number 0-2
 
@@ -21,33 +18,23 @@ const cpuChoice = getComputerChoice();
 getComputerChoice()
 console.log(cpuChoice)
 
+
+
 function getHumanChoice(){
    let humanChoice=prompt("Rock, Paper, Scissors?").toLowerCase(); //Ask player for choice, convert to lower case
 
     //If user picks Rock
-    if (humanChoice === "rock" && cpuChoice === "Rock"){
-        return "You Tied! Zero points!"}
-    else if (humanChoice === "rock" && cpuChoice === "Paper"){
-        return "Paper beats rock! 1 point to CPU!"}
-    else if (humanChoice === "rock" && cpuChoice === "Scissors"){
-        return "Rock beats scissors! 1 point to Player!" }
-
+    if (humanChoice === "rock"){
+        return "Rock";
+    }
     //User picks Paper
-    else if (humanChoice === "paper" && cpuChoice === "Paper"){
-        return "You Tied! Zero points!"}
-    else if (humanChoice === "paper" && cpuChoice === "Scissors"){
-        return "Scissors beats paper! 1 point to CPU!" }
-    else if (humanChoice === "paper" && cpuChoice === "Rock"){
-        return "Paper beats rock! 1 point to Player!"}
-
+    else if (humanChoice === "paper" ){
+        return "Paper";}
+    
     //User picks Scissors
-    else if (humanChoice === "scissors" && cpuChoice === "Paper"){
-        return "Scissors beats paper! 1 point to Player!"}
-    else if (humanChoice === "scissors" && cpuChoice === "Scissors"){
-        return "You Tied! Zero points!" }
-    else if (humanChoice === "scissors" && cpuChoice === "Rock"){
-        return "Rock beats scissors! 1 point to CPU!" }
-
+    else if (humanChoice === "scissors"){
+        return "Scissors"}
+   
     //String unrecognized
     else {
         return "You must pick rock, paper, or scissors!"
@@ -55,4 +42,8 @@ function getHumanChoice(){
 }
 const humanChoice = getHumanChoice();
 console.log(humanChoice)
+
+let humanScore;
+let computerScore;
+
 
