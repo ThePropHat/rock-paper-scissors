a9b2c2f2205a6d6fcc1ae3ca2ae6869e363d903e
ThePropHat <85534637+ThePropHat@users.noreply.github.com>
2025-07-27 15:29:48 -0600

    Implement choice system

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,7 @@
+
+
+
+
 function getComputerChoice(){
     const randomNumber = Math.floor(Math.random() * 3); //Generate random number 0-2
 
@@ -13,26 +17,42 @@ function getComputerChoice(){
         return "Paper";
     }
 }
-
+const cpuChoice = getComputerChoice();
 getComputerChoice()
 console.log(cpuChoice)
 
 function getHumanChoice(){
-    let humanChoice = prompt("Rock, Paper, Scissors?"); //Ask player for choice
+   let humanChoice=prompt("Rock, Paper, Scissors?").toLowerCase(); //Ask player for choice, convert to lower case
+
+    //If user picks Rock
     if (humanChoice === "rock" && cpuChoice === "Rock"){
-        return "You Tied! Zero points!"
-    }
+        return "You Tied! Zero points!"}
     else if (humanChoice === "rock" && cpuChoice === "Paper"){
         return "Paper beats rock! 1 point to CPU!"}
-
     else if (humanChoice === "rock" && cpuChoice === "Scissors"){
-        return "Rock beats scissors! 1 point to Player!" 
-    }
+        return "Rock beats scissors! 1 point to Player!" }
 
+    //User picks Paper
+    else if (humanChoice === "paper" && cpuChoice === "Paper"){
+        return "You Tied! Zero points!"}
+    else if (humanChoice === "paper" && cpuChoice === "Scissors"){
+        return "Scissors beats paper! 1 point to CPU!" }
+    else if (humanChoice === "paper" && cpuChoice === "Rock"){
+        return "Paper beats rock! 1 point to Player!"}
+
+    //User picks Scissors
+    else if (humanChoice === "scissors" && cpuChoice === "Paper"){
+        return "Scissors beats paper! 1 point to Player!"}
+    else if (humanChoice === "scissors" && cpuChoice === "Scissors"){
+        return "You Tied! Zero points!" }
+    else if (humanChoice === "scissors" && cpuChoice === "Rock"){
+        return "Rock beats scissors! 1 point to CPU!" }
+
+    //String unrecognized
+    else {
+        return "You must pick rock, paper, or scissors!"
+        }
 }
-
-getHumanChoice()
+const humanChoice = getHumanChoice();
 console.log(humanChoice)
 
-const cpuChoice = getComputerChoice();
-const humanChoice = getHumanChoice();
