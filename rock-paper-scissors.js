a8a3146e6fdb7776e38a9dbb595a7e3f952ca71f
ThePropHat <85534637+ThePropHat@users.noreply.github.com>
2025-07-28 10:02:54 -0600

    Implement game logic

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,49 +1,45 @@
 
 function getComputerChoice(){
-    const randomNumber = Math.floor(Math.random() * 3); //Generate random number 0-2
-
+    let randomNumber = Math.floor(Math.random() * 3); //Generate random number 0-2
+    
     if (randomNumber===0){
-        return "Rock";
+        compChoice = "rock";
     }
 
     else if (randomNumber===1){
-        return "Scissors";
+        compChoice = "Paper";
     }
    
     else{
-        return "Paper";
+        compChoice = "Scissors";
     }
+    return compChoice
 }
-const cpuChoice = getComputerChoice();
-getComputerChoice()
-console.log(cpuChoice)
-
-
 
 function getHumanChoice(){
-   let humanChoice=prompt("Rock, Paper, Scissors?").toLowerCase(); //Ask player for choice, convert to lower case
-
-    //If user picks Rock
-    if (humanChoice === "rock"){
-        return "Rock";
-    }
-    //User picks Paper
-    else if (humanChoice === "paper" ){
-        return "Paper";}
-    
-    //User picks Scissors
-    else if (humanChoice === "scissors"){
-        return "Scissors"}
-   
-    //String unrecognized
-    else {
-        return "You must pick rock, paper, or scissors!"
-        }
+    let humanChoice = prompt("Rock, Paper, Scissors?").toLowerCase(); //Ask player for choice, convert to lower case
+    return humanChoice
 }
-const humanChoice = getHumanChoice();
-console.log(humanChoice)
 
-let humanScore;
-let computerScore;
+function playGame(humanChoice, compChoice){
+    let humanScore = 0
+    let cpuScore = 0
+
+    if (humanChoice === "rock" && compChoice === "scissors" || humanChoice === "scissors" && compChoice === "paper" || humanChoice === "paper" && compChoice === "rock"){
+        console.log(`${humanChoice} beats ${compChoice}! You get 1 point!`)
+        ++humanScore;}
+
+    else if (humanChoice === "scissors" && compChoice === "rock" || humanChoice === "paper" && compChoice === "scissors" || humanChoice === "rock" && compChoice === "paper"){
+        console.log(`${humanChoice} is beat by ${compChoice}! Computer gets 1 point!`)
+        ++cpuScore;}
+
+   else if (humanChoice===compChoice){
+    console.log(`${humanChoice} is the same as ${compChoice}! No points awarded!`)}
+
+   else{
+    console.log("Please type rock, paper, or scissors.")
+   }
+
+}
 
 
