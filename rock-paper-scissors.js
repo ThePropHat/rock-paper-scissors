02ef6362bb3414e0609615b2fe40c20ad46476b8
ThePropHat <85534637+ThePropHat@users.noreply.github.com>
2025-08-11 12:18:04 -0600

    Reintegrate logic into new round function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -18,37 +18,43 @@ function getClankerChoice(){
 
 }
 
-const clankerChoice = getClankerChoice()
-getClankerChoice()
-console.log("the clanker has picked ", clankerChoice)
-
 function getUserChoice(){
-    const userChoice = prompt("Rock, paper, scissor?");
-    const lowerUserChoice = userChoice.toLowerCase();
+    const userInput = prompt("Rock, paper, scissor?");
+    const userChoice = userInput.toLowerCase();
+}
 
-    if ((lowerUserChoice === "rock" && clankerChoice === "scissors" ) || 
-        (lowerUserChoice === "scissors" && clankerChoice === "paper") ||
-        (lowerUserChoice === "paper" && clankerChoice === "rock")){
-        return "you picked " + userChoice + ". you win! here, have a point!"
+
+function playRound(userChoice, clankerChoice){
+    console.log("the clanker has picked ", clankerChoice)
+      
+    if ((userChoice === "rock" && clankerChoice === "scissors" ) || //player wins
+        (userChoice === "scissors" && clankerChoice === "paper") ||
+        (userChoice === "paper" && clankerChoice === "rock")){
+             humanScore++
+        console.log( "you picked " + userChoice + ". you win! here, have a point! | you: " + humanScore + " - clanker: " + clankerScore)
+       
+        
     }
 
-    else if((lowerUserChoice === "scissors" && clankerChoice === "rock" ) || 
-        (lowerUserChoice === "paper" && clankerChoice === "scissors") ||
-        (lowerUserChoice === "rock" && clankerChoice === "paper")){
-            return "you picked " + userChoice + ". you lost...the clanker gets a point"
-
+    else if((userChoice === "scissors" && clankerChoice === "rock" ) ||  //player loses
+        (userChoice === "paper" && clankerChoice === "scissors") ||
+        (userChoice === "rock" && clankerChoice === "paper")){
+                clankerScore++
+            console.log( "you picked " + userChoice + ". you lost...the clanker gets a point. | you: " + humanScore + " - clanker: " + clankerScore)
+            
     }
 
-    else if(lowerUserChoice === clankerChoice){
-        return "you and the clanker tied! are you cheating?"
+    else if(userChoice === clankerChoice){ //player input = clanker
+        console.log( "you and the clanker tied! are you cheating? | you: " + humanScore + " - clanker: " + clankerScore)
     }
 
-    else{
-        return "please pick rock paper or scissors...."
+    else{ //bad input
+        console.log( "please pick rock paper or scissors.... | you: " + humanScore + " - clanker: " + clankerScore)
     }
-
 
 }
 
-const userChoice = getUserChoice()
-console.log(userChoice)
+const userChoice = getUserChoice();
+const clankerChoice = getClankerChoice()
+
+playRound(userChoice, clankerChoice);
