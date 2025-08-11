590befbb863e25b34006a73308c9941a77293494
ThePropHat <85534637+ThePropHat@users.noreply.github.com>
2025-08-11 11:00:47 -0600

    Generate computer choice as string value

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,9 +2,21 @@
 
 function getComputerChoice(){
     let choice = Math.floor(Math.random()*(3) + 1);
-    console.log(choice);
     
+    if (choice === 1){
+        return "Rock"
+    }
+
+    else if(choice === 2){
+        return "Paper"
+    }
+
+    else if(choice === 3){
+        return "Scissors"
+    }
 
 }
 
+const compChoice = getComputerChoice()
 getComputerChoice()
+console.log(compChoice)
