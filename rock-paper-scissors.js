2aad17284aea597b3f6dfccdfc92dd4716a87767
ThePropHat <85534637+ThePropHat@users.noreply.github.com>
2025-08-11 11:27:45 -0600

    Logic player selection

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,22 +1,54 @@
+let humanScore=0;
+let clankerScore=0;
 
-
-function getComputerChoice(){
+function getClankerChoice(){
     let choice = Math.floor(Math.random()*(3) + 1);
     
     if (choice === 1){
-        return "Rock"
+        return "rock"
     }
 
     else if(choice === 2){
-        return "Paper"
+        return "paper"
     }
 
     else if(choice === 3){
-        return "Scissors"
+        return "scissors"
     }
 
 }
 
-const compChoice = getComputerChoice()
-getComputerChoice()
-console.log(compChoice)
+const clankerChoice = getClankerChoice()
+getClankerChoice()
+console.log("the clanker has picked ", clankerChoice)
+
+function getUserChoice(){
+    const userChoice = prompt("Rock, paper, scissor?");
+    const lowerUserChoice = userChoice.toLowerCase();
+
+    if ((lowerUserChoice === "rock" && clankerChoice === "scissors" ) || 
+        (lowerUserChoice === "scissors" && clankerChoice === "paper") ||
+        (lowerUserChoice === "paper" && clankerChoice === "rock")){
+        return "you picked " + userChoice + ". you win! here, have a point!"
+    }
+
+    else if((lowerUserChoice === "scissors" && clankerChoice === "rock" ) || 
+        (lowerUserChoice === "paper" && clankerChoice === "scissors") ||
+        (lowerUserChoice === "rock" && clankerChoice === "paper")){
+            return "you picked " + userChoice + ". you lost...the clanker gets a point"
+
+    }
+
+    else if(lowerUserChoice === clankerChoice){
+        return "you and the clanker tied! are you cheating?"
+    }
+
+    else{
+        return "please pick rock paper or scissors...."
+    }
+
+
+}
+
+const userChoice = getUserChoice()
+console.log(userChoice)
